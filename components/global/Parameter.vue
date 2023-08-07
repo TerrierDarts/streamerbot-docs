05c9f9163aeb1960f32be54a2048fe94c19b4fc5
Whipstickgostop <8366326+Whipstickgostop@users.noreply.github.com>
2023-08-07 10:42:46 -0400

    add obs connection param

---
+++ components/global/Parameter.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props = defineProps<{name?: string}>();
+
+const { data, pending } = useAsyncData(`parameter-${props.name ?? 'empty'}`, () => {
+  if (!props.name) return Promise.resolve(null);
+  return queryContent('api', '_parameters').where({
+    _partial: true,
+    $or: [
+      { 'parameter.name': props.name ?? '' },
+      { 'title': props.name ?? '' },
+    ]
+  }).findOne();
+});
+</script>
+
+<template>
+  <List v-if="!name" type="danger">
+    Unknown Parameter
+  </List>
+  <ContentRenderer v-else-if="data" :value="data">
+    <template #empty>
+      <List type="warning">
+        Parameter not found
+      </List>
+    </template>
+  </ContentRenderer>
+  <List v-else-if="!pending" type="danger">
+    Unknown Parameter
+  </List>
+</template>
